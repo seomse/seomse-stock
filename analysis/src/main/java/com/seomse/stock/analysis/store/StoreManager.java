5bc8f361eb284c2aacebd687896035a37fb63d69
macle <ysys86a@gmail.com>
2020-12-09 01:09:17 +0900

    lock 구조 세분하

--- analysis/src/main/java/com/seomse/stock/analysis/store/StoreManager.java
+++ analysis/src/main/java/com/seomse/stock/analysis/store/StoreManager.java
@@ -36,6 +36,8 @@ public class StoreManager {
 
     //개별종목
     private final Map<String, ItemStore> itemStoreMap = new HashMap<>();
+
+    private final Map<String, Object> itemYmdLockMap = new HashMap<>();
     private final Object itemLock = new Object();
 
     /**
@@ -46,7 +48,13 @@ public class StoreManager {
     public ItemStore getItemStore(String ymd){
         ItemStore itemStore = itemStoreMap.get(ymd);
         if(itemStore == null){
+
+            Object ymdLock;
             synchronized (itemLock){
+                ymdLock = itemYmdLockMap.computeIfAbsent(ymd, k -> new Object());
+            }
+
+            synchronized (ymdLock){
                 //lock 구간에서 다시 체크
                 itemStore = itemStoreMap.get(ymd);
                 if(itemStore == null){
@@ -61,6 +69,7 @@ public class StoreManager {
 
     //우선주
     private final Map<String, PreferredStore> preferredStoreMap = new HashMap<>();
+    private final Map<String, Object> preferredYmdLockMap = new HashMap<>();
     private final Object preferredLock = new Object();
 
     /**
@@ -72,7 +81,14 @@ public class StoreManager {
         ItemStore itemStore = getItemStore(ymd);
         PreferredStore preferredStore = preferredStoreMap.get(ymd);
         if(preferredStore== null){
+
+            Object ymdLock;
             synchronized (preferredLock){
+                ymdLock = preferredYmdLockMap.computeIfAbsent(ymd, k -> new Object());
+            }
+
+            //lock 객체를 년원일 별로 관리하기
+            synchronized (ymdLock){
                 preferredStore = preferredStoreMap.get(ymd);
                 if(preferredStore == null){
                     preferredStore = new PreferredStore(itemStore);
@@ -86,6 +102,8 @@ public class StoreManager {
     
     //etf
     private final Map<String, EtfStore> etfStoreMap = new HashMap<>();
+    private final Map<String, Object> etfYmdLockMap = new HashMap<>();
+
     private final Object etfLock = new Object();
 
     /**
@@ -97,7 +115,13 @@ public class StoreManager {
         EtfStore etfStore= etfStoreMap.get(ymd);
        
         if(etfStore == null){
+            Object ymdLock;
             synchronized (etfLock){
+                ymdLock = etfYmdLockMap.computeIfAbsent(ymd, k -> new Object());
+            }
+
+
+            synchronized (ymdLock){
                 etfStore= etfStoreMap.get(ymd);
                 if(etfStore == null){
                     etfStore =new EtfStore(ymd);
@@ -110,6 +134,7 @@ public class StoreManager {
     
     //국내증시
     private final Map<String, DomesticMarketStore> domesticMarketStoreMap = new HashMap<>();
+    private final Map<String, Object> domesticMarketYmdLockMap = new HashMap<>();
     private final Object domesticMarketLock = new Object();
 
     /**
@@ -121,7 +146,13 @@ public class StoreManager {
         DomesticMarketStore domesticMarketStore = domesticMarketStoreMap.get(ymd);
         
         if(domesticMarketStore == null){
+
+            Object ymdLock;
             synchronized (domesticMarketLock){
+                ymdLock = domesticMarketYmdLockMap.computeIfAbsent(ymd, k -> new Object());
+            }
+
+            synchronized (ymdLock){
                 domesticMarketStore = domesticMarketStoreMap.get(ymd);
                 if(domesticMarketStore == null){
                     domesticMarketStore = new DomesticMarketStore(ymd);
@@ -136,6 +167,7 @@ public class StoreManager {
 
     //해외증시
     private final Map<String, OverseasMarketStore> overseasMarketStoreMap = new HashMap<>();
+    private final Map<String, Object> overseasMarketYmdLockMap = new HashMap<>();
     private final Object overseasMarketLock = new Object();
     /**
      * 해외증시 얻기
@@ -146,7 +178,15 @@ public class StoreManager {
         OverseasMarketStore overseasMarketStore =  overseasMarketStoreMap.get(ymd);
 
         if(overseasMarketStore == null){
+
+
+            Object ymdLock;
             synchronized (overseasMarketLock){
+                ymdLock = overseasMarketYmdLockMap.computeIfAbsent(ymd, k -> new Object());
+            }
+
+
+            synchronized (ymdLock){
                 overseasMarketStore = overseasMarketStoreMap.get(ymd);
                 if(overseasMarketStore == null){
                     overseasMarketStore = new OverseasMarketStore(ymd);
@@ -160,6 +200,7 @@ public class StoreManager {
     
     //시장지수
     private final Map<String, MarketIndexStore> indexStoreMap = new HashMap<>();
+    private final Map<String, Object> indexYmdLockMap = new HashMap<>();
     private final Object indexStoreLock = new Object();
 
     /**
@@ -170,7 +211,12 @@ public class StoreManager {
     public MarketIndexStore getMarketIndexStore(String ymd){
         MarketIndexStore marketIndexStore = indexStoreMap.get(ymd);
         if(marketIndexStore == null){
+
+            Object ymdLock;
             synchronized (indexStoreLock){
+                ymdLock = indexYmdLockMap.computeIfAbsent(ymd, k -> new Object());
+            }
+            synchronized (ymdLock){
                 marketIndexStore = indexStoreMap.get(ymd);
                 if(marketIndexStore == null){
                     marketIndexStore = new MarketIndexStore(ymd);
@@ -185,6 +231,7 @@ public class StoreManager {
     }
 
     private final Map<String, WicsStore> wicsStoreMap = new HashMap<>();
+    private final Map<String, Object> wicsYmdLockMap = new HashMap<>();
     private final Object wicsStoreLock = new Object();
 
     /**
@@ -195,7 +242,13 @@ public class StoreManager {
     public WicsStore getWicsStore(String ymd){
         WicsStore wicsStore = wicsStoreMap.get(ymd);
         if(wicsStore == null){
+
+            Object ymdLock;
             synchronized (wicsStoreLock){
+                ymdLock = wicsYmdLockMap.computeIfAbsent(ymd, k -> new Object());
+            }
+
+            synchronized (ymdLock){
                 wicsStore = wicsStoreMap.get(ymd);
                 if(wicsStore == null){
                     wicsStore = new WicsStore(ymd);
@@ -206,6 +259,10 @@ public class StoreManager {
         return wicsStore;
     }
 
+    // 테마는 삭제하지 않음
+    // 아직 많지 않은 정보를 사용하는 것.
+    // 과거 테마정보는 없어서 정보의 오차가 있을 수 있음
+
     /**
      * 일자에 해당하는 store 제거( 메모리 관리)
      * @param ymd  yyyyMMdd
@@ -213,24 +270,31 @@ public class StoreManager {
     public void remove(String ymd){
         synchronized (itemLock){
             itemStoreMap.remove(ymd);
+            itemYmdLockMap.remove(ymd);
         }
         synchronized (preferredLock){
             preferredStoreMap.remove(ymd);
+            preferredYmdLockMap.remove(ymd);
         }
         synchronized (etfLock){
             etfStoreMap.remove(ymd);
+            etfYmdLockMap.remove(ymd);
         }
         synchronized (domesticMarketLock){
             domesticMarketStoreMap.remove(ymd);
+            domesticMarketYmdLockMap.remove(ymd);
         }
         synchronized (overseasMarketLock){
             overseasMarketStoreMap.remove(ymd);
+            overseasMarketYmdLockMap.remove(ymd);
         }
         synchronized (indexStoreLock){
             indexStoreMap.remove(ymd);
+            indexYmdLockMap.remove(ymd);
         }
         synchronized (wicsStoreLock){
             wicsStoreMap.remove(ymd);
+            wicsYmdLockMap.remove(ymd);
         }
     }
 
