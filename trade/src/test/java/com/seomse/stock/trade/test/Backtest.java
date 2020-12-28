39bd63a124c1aa22f5e8772e5a2c99f2e720d7a2
macle <ysys86a@gmail.com>
2020-12-28 23:27:27 +0900

    다이버젼스 작업중

--- trade/src/test/java/com/seomse/stock/trade/test/Backtest.java
+++ trade/src/test/java/com/seomse/stock/trade/test/Backtest.java
@@ -41,28 +41,6 @@ public class Backtest {
         final StoreManager storeManager = new StoreManager();
         List<String> ymdList = JdbcQuery.getStringList("SELECT YMD FROM T_STOCK_MARKET_DAILY WHERE MARKET_CD ='KOSPI' AND YMD >= '" +beginYmd +"' AND YMD <= '" + endYmd +"' ORDER BY YMD ASC");
         
-        
-        //메모리 상에 올려놓고 여러전략을 한번에 테스트하는게 아니면 의미가 없어서 제외함
-        //하나의 전략 테스트에서는 의미가 없음
-//        new Thread(() -> {
-            //분석속도 차이가 클떄만
-            //거래일 얻기
-            //거래일은 코스피 지수의 거래가 있는날짜로 한다
-            ymdList.remove(0);
-            //2번째 메모리 부터 미리 생성해놓기
-            for(String ymd : ymdList){
-                System.out.println("*************************************** ymd memory create: " + ymd);
-                //메모리 정보 미리 생성하기 (사용하는 정보만)
-                storeManager.getItemStore(ymd);
-                storeManager.getPreferredStore(ymd);
-                storeManager.getEtfStore(ymd);
-                storeManager.getDomesticMarketStore(ymd);
-//                storeManager.getOverseasMarketStore(ymd);
-//                storeManager.getMarketIndexStore(ymd);
-//                storeManager.getWicsStore(ymd);
-            }
-//        }).start();
-//
 
         TestBuyStrategy testBuyStrategy = new TestBuyStrategy(storeManager);
         TestSellStrategy testSellStrategy = new TestSellStrategy(storeManager);
